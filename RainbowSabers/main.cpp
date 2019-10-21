66b970606e342f9feb71d27b3f5b78b9cd93fbd4
darknight1050 <darkdeveloping1050@gmail.com>
2019-10-21 07:38:05 +0200

    Added Rainbow Walls and Tutorial Fix
    In the tutorial everything gets disabled

--- RainbowSabers/main.cpp
+++ RainbowSabers/main.cpp
@@ -22,20 +22,27 @@
 #include "../beatsaber-hook/shared/utils/typedefs.h"
 
 #define SaberBurnMarkSparkles_LateUpdate_Offset 0xA0A5BC
-
 #define GameNoteController_Update_Offset 0xA535F8
+#define ObstacleController_Update_Offset 0xCE044C
+#define TutorialController_Awake_Offset 0xB2F4C8
+#define TutorialController_OnDestroy_Offset 0xB2F944
+
+using namespace il2cpp_utils;
+using namespace il2cpp_functions;
 
 static auto& config_doc = Configuration::config;
 
 static struct Config_t {
 	bool SabersActive = true;
 	bool LightsActive = true;
+	bool WallsActive = true;
 	double SaberASpeed = 1.0;
 	double SaberBSpeed = 1.0;
 	double SabersStartDiff = 180.0;
 	double LightASpeed = 2.0;
 	double LightBSpeed = 2.0;
 	double LightsStartDiff = 180.0;
+	double WallsSpeed = 2.0;
 } Config;
 
 typedef struct {
@@ -49,142 +56,186 @@ typedef struct {
 	Color obstaclesColor;
 } ColorScheme;
 
+ColorScheme colorScheme;
+
 static float saberA = 0;
 static float saberB = 0;
 static float environmentColor0 = 0;
 static float environmentColor1 = 0;
 static float obstaclesColor = 0;
-
-Color ColorFormHSV(float H, float S, float V) {
-	float C = S * V;
-	float X = C * (1 - abs(fmod(H / 60.0f, 2) - 1));
-	float m = V - C;
-	float Rs, Gs, Bs;
-
-	if(H >= 0 && H < 60) {
-		Rs = C;
-		Gs = X;
-		Bs = 0;	
+static bool InTutorial = false; 
+Color ColorFromHSB(float hue, float saturation, float brightness){
+	hue/=360.0f;
+	int r = 0, g = 0, b = 0;
+	if (saturation == 0)
+	{
+		r = g = b = (int)(brightness * 255.0f + 0.5f);
 	}
-	else if(H >= 60 && H < 120) {	
-		Rs = X;
-		Gs = C;
-		Bs = 0;	
-	}
-	else if(H >= 120 && H < 180) {
-		Rs = 0;
-		Gs = C;
-		Bs = X;	
-	}
-	else if(H >= 180 && H < 240) {
-		Rs = 0;
-		Gs = X;
-		Bs = C;	
-	}
-	else if(H >= 240 && H < 300) {
-		Rs = X;
-		Gs = 0;
-		Bs = C;	
-	}
-	else {
-		Rs = C;
-		Gs = 0;
-		Bs = X;	
-	}
-	Color color;
-	color.r = (Rs + m);
-	color.g = (Gs + m);
-	color.b = (Bs + m);
+	else
+	{
+		float h = (hue - (float)floor (hue)) * 6.0f;
+		float f = h - (float)floor (h);
+        float p = brightness * (1.0f - saturation);
+        float q = brightness * (1.0f - saturation * f);
+        float t = brightness * (1.0f - (saturation * (1.0f - f)));
+		switch ((int)h)
+        {
+			case 0:
+				r = (int)(brightness * 255.0f + 0.5f);
+				g = (int)(t * 255.0f + 0.5f);
+                b = (int)(p * 255.0f + 0.5f);
+                break;
+			case 1:
+				r = (int)(q * 255.0f + 0.5f);
+				g = (int)(brightness * 255.0f + 0.5f);
+				b = (int)(p * 255.0f + 0.5f);
+				break;
+			case 2:
+				r = (int)(p * 255.0f + 0.5f);
+				g = (int)(brightness * 255.0f + 0.5f);
+				b = (int)(t * 255.0f + 0.5f);
+				break;
+			case 3:
+				r = (int)(p * 255.0f + 0.5f);
+				g = (int)(q * 255.0f + 0.5f);
+				b = (int)(brightness * 255.0f + 0.5f);
+				break;
+			case 4:
+				r = (int)(t * 255.0f + 0.5f);
+				g = (int)(p * 255.0f + 0.5f);
+				b = (int)(brightness * 255.0f + 0.5f);
+				break;
+			case 5:
+				r = (int)(brightness * 255.0f + 0.5f);
+				g = (int)(p * 255.0f + 0.5f);
+				b = (int)(q * 255.0f + 0.5f);
+				break;
+		}
+    }
+    Color color;
+	color.r = r/255.0f;
+	color.g = g/255.0f;
+	color.b = b/255.0f;
 	color.a = 1.0f;
 	return color;
 }
 
-Color getColorFromManager(Il2CppObject* color){
-	auto klass = il2cpp_functions::object_get_class(color);
-	auto field = il2cpp_functions::class_get_field_from_name(klass, "_color");
-	Color saberColor;
-	il2cpp_functions::field_get_value(color, field, &saberColor);
-	return saberColor;
-}
-
 Color getColorFromManager(Il2CppObject* colorManager, const char* fieldName){
-	auto klass = il2cpp_functions::object_get_class(colorManager);
-	auto fieldColorManager = il2cpp_functions::class_get_field_from_name(klass, fieldName);
-	Il2CppObject* color = il2cpp_functions::field_get_value_object(fieldColorManager, colorManager);
-	return getColorFromManager(color);
+	Color color;
+	GetFieldValue(&color, GetFieldObjectValue(colorManager, fieldName), "_color");
+	return color; 
 }
 
 MAKE_HOOK(SaberBurnMarkSparkles_LateUpdate, SaberBurnMarkSparkles_LateUpdate_Offset, void, Il2CppObject* self, void *type){
-	il2cpp_functions::Init();
-	SaberBurnMarkSparkles_LateUpdate(self, type);
-	auto klass = il2cpp_functions::object_get_class(self);
-	auto fieldColorManager = il2cpp_functions::class_get_field_from_name(klass, "_colorManager");
-	Il2CppObject* colorManager = il2cpp_functions::field_get_value_object(fieldColorManager, self);
-	
-	saberA+=Config.SaberASpeed;
-	if(saberA > 360){
-		saberA = 0;
-	}
-	saberB+=Config.SaberBSpeed;
-	if(saberB > 360){
-		saberB = 0;
-	}
-	environmentColor0+=Config.LightASpeed;
-	if(environmentColor0 > 360){
-		environmentColor0 = 0;
-	}
-	environmentColor1+=Config.LightBSpeed;
-	if(environmentColor1 > 360){
-		environmentColor1 = 0;
-	}
-	obstaclesColor+=1;
-	if(obstaclesColor > 360){
-		obstaclesColor = 0;
-	}
-	ColorScheme colorScheme;
-	if(Config.SabersActive){
-		colorScheme.saberAColor = ColorFormHSV(saberA, 1.0, 1.0);
-		colorScheme.saberBColor = ColorFormHSV(saberB, 1.0, 1.0);
-	}else{
+	Init();
+	Il2CppObject* colorManager = GetFieldObjectValue(self, "_colorManager");
+	if(InTutorial){
 		colorScheme.saberAColor = getColorFromManager(colorManager, "_saberAColor");
 		colorScheme.saberBColor = getColorFromManager(colorManager, "_saberBColor");
-	}
-	if(Config.LightsActive){
-		colorScheme.environmentColor0 = ColorFormHSV(environmentColor0, 1.0, 1.0);
-		colorScheme.environmentColor1 = ColorFormHSV(environmentColor1, 1.0, 1.0);
-	}else{
 		colorScheme.environmentColor0 = getColorFromManager(colorManager, "_environmentColor0");
 		colorScheme.environmentColor1 = getColorFromManager(colorManager, "_environmentColor1");
+		colorScheme.obstaclesColor = getColorFromManager(colorManager, "_obstaclesColor");
+	}else{
+		saberA+=Config.SaberASpeed;
+		if(saberA > 360){
+			saberA = 0;
+		}
+		saberB+=Config.SaberBSpeed;
+		if(saberB > 360){
+			saberB = 0;
+		}
+		environmentColor0+=Config.LightASpeed;
+		if(environmentColor0 > 360){
+			environmentColor0 = 0;
+		}
+		environmentColor1+=Config.LightBSpeed;
+		if(environmentColor1 > 360){
+			environmentColor1 = 0;
+		}
+		obstaclesColor+=Config.WallsSpeed;
+		if(obstaclesColor > 360){
+			obstaclesColor = 0;
+		}
+		if(Config.SabersActive){
+			colorScheme.saberAColor = ColorFromHSB(saberA, 1.0, 1.0);
+			colorScheme.saberBColor = ColorFromHSB(saberB, 1.0, 1.0);
+		}else{
+			colorScheme.saberAColor = getColorFromManager(colorManager, "_saberAColor");
+			colorScheme.saberBColor = getColorFromManager(colorManager, "_saberBColor");
+		}
+		if(Config.LightsActive){
+			colorScheme.environmentColor0 = ColorFromHSB(environmentColor0, 1.0, 1.0);
+			colorScheme.environmentColor1 = ColorFromHSB(environmentColor1, 1.0, 1.0);
+		}else{
+			colorScheme.environmentColor0 = getColorFromManager(colorManager, "_environmentColor0");
+			colorScheme.environmentColor1 = getColorFromManager(colorManager, "_environmentColor1");
+		}
+		
+		if(Config.WallsActive){
+			colorScheme.obstaclesColor = ColorFromHSB(obstaclesColor, 1.0, 1.0);
+		}else{
+			colorScheme.obstaclesColor = getColorFromManager(colorManager, "_obstaclesColor");
+		}
 	}
-	colorScheme.obstaclesColor = getColorFromManager(colorManager, "_obstaclesColor");
-	il2cpp_utils::RunMethod(colorManager, "SetColorScheme", &colorScheme);
-  
+	RunMethod(colorManager, "SetColorScheme", &colorScheme);
+	SaberBurnMarkSparkles_LateUpdate(self, type);
 }
 
 MAKE_HOOK(GameNoteController_Update, GameNoteController_Update_Offset, void, Il2CppObject* self){
-	auto klass = il2cpp_functions::object_get_class(self);
-	auto fielDisappearingArrowController = il2cpp_functions::class_get_field_from_name(klass, "_disappearingArrowController");
-	Il2CppObject* disappearingArrowController = il2cpp_functions::field_get_value_object(fielDisappearingArrowController, self);
-	auto disappearingArrowControllerClass = il2cpp_functions::object_get_class(disappearingArrowController);
-	auto fielColorNoteVisuals = il2cpp_functions::class_get_field_from_name(disappearingArrowControllerClass, "_colorNoteVisuals");
-	Il2CppObject* colorNoteVisuals = il2cpp_functions::field_get_value_object(fielColorNoteVisuals, disappearingArrowController);
-	auto colorNoteVisualsClass = il2cpp_functions::object_get_class(colorNoteVisuals);
-	auto fieldNoteController = il2cpp_functions::class_get_field_from_name(colorNoteVisualsClass, "_noteController");
-	Il2CppObject* noteController = il2cpp_functions::field_get_value_object(fieldNoteController, colorNoteVisuals);
-	il2cpp_utils::RunMethod(colorNoteVisuals, "HandleNoteControllerDidInitEvent", noteController);
+	Init();
+	Il2CppObject* disappearingArrowController = GetFieldObjectValue(self, "_disappearingArrowController");
+	Il2CppObject* colorNoteVisuals = GetFieldObjectValue(disappearingArrowController, "_colorNoteVisuals");
+	Il2CppObject* noteController = GetFieldObjectValue(colorNoteVisuals, "_noteController");
+	RunMethod(colorNoteVisuals, "HandleNoteControllerDidInitEvent", noteController);
 	GameNoteController_Update(self);
+}
+
+MAKE_HOOK(ObstacleController_Update, ObstacleController_Update_Offset, void, Il2CppObject* self){
+	Init();
+	Il2CppObject* stretchableObstacle = GetFieldObjectValue(self, "_stretchableObstacle");
+	Il2CppObject* obstacleFrame = GetFieldObjectValue(stretchableObstacle, "_obstacleFrame");
+	
+	float width, height, length;
+	GetFieldValue(&width, obstacleFrame, "width");
+	GetFieldValue(&height, obstacleFrame, "height");
+	GetFieldValue(&length, obstacleFrame, "length");
+	RunMethod(stretchableObstacle, "SetSizeAndColor", &width, &height, &length, &colorScheme.obstaclesColor);
+	ObstacleController_Update(self);
+}
+
+MAKE_HOOK(TutorialController_Awake, TutorialController_Awake_Offset, void, Il2CppObject* self){
+	TutorialController_Awake(self);
+	InTutorial = true;
+}
+
+MAKE_HOOK(TutorialController_OnDestroy, TutorialController_OnDestroy_Offset, void, Il2CppObject* self){
+	TutorialController_OnDestroy(self);
+	InTutorial = false;
 }
 
 void createDefaultConfig() {
 	log(INFO, "Creating Configuration...");
+	config_doc.RemoveMember("SabersActive");
+	config_doc.RemoveMember("LightsActive");
+	config_doc.RemoveMember("WallsActive");
+	config_doc.RemoveMember("SaberASpeed");
+	config_doc.RemoveMember("SaberBSpeed");
+	config_doc.RemoveMember("SabersStartDiff");
+	config_doc.RemoveMember("LightASpeed");
+	config_doc.RemoveMember("LightBSpeed");
+	config_doc.RemoveMember("LightsStartDiff");
+	config_doc.RemoveMember("WallsSpeed");
+	
 	config_doc.AddMember("SabersActive", Config.SabersActive, config_doc.GetAllocator());
 	config_doc.AddMember("LightsActive", Config.LightsActive, config_doc.GetAllocator());
+	config_doc.AddMember("WallsActive", Config.WallsActive, config_doc.GetAllocator());
 	config_doc.AddMember("SaberASpeed", Config.SaberASpeed, config_doc.GetAllocator());
 	config_doc.AddMember("SaberBSpeed", Config.SaberBSpeed, config_doc.GetAllocator());
 	config_doc.AddMember("SabersStartDiff", Config.SabersStartDiff, config_doc.GetAllocator());
 	config_doc.AddMember("LightASpeed", Config.LightASpeed, config_doc.GetAllocator());
 	config_doc.AddMember("LightBSpeed", Config.LightBSpeed, config_doc.GetAllocator());
 	config_doc.AddMember("LightsStartDiff", Config.LightsStartDiff, config_doc.GetAllocator());
+    config_doc.AddMember("WallsSpeed", Config.WallsSpeed, config_doc.GetAllocator());
     Configuration::Write();
 	log(INFO, "Created Configuration!");
 }
@@ -194,20 +245,24 @@ bool loadConfig() {
 	Configuration::Load();
 	if(config_doc.HasMember("SabersActive") && config_doc["SabersActive"].IsBool() &&
 		config_doc.HasMember("LightsActive") && config_doc["LightsActive"].IsBool() &&
+		config_doc.HasMember("WallsActive") && config_doc["WallsActive"].IsBool() &&
 		config_doc.HasMember("SaberASpeed") && config_doc["SaberASpeed"].IsDouble() &&
 		config_doc.HasMember("SaberBSpeed") && config_doc["SaberBSpeed"].IsDouble() && 
 		config_doc.HasMember("SabersStartDiff") && config_doc["SabersStartDiff"].IsDouble() && 
 		config_doc.HasMember("LightASpeed") && config_doc["LightASpeed"].IsDouble() &&
 		config_doc.HasMember("LightBSpeed") && config_doc["LightBSpeed"].IsDouble() &&
-		config_doc.HasMember("LightsStartDiff") && config_doc["LightsStartDiff"].IsDouble()){
+		config_doc.HasMember("LightsStartDiff") && config_doc["LightsStartDiff"].IsDouble() &&
+		config_doc.HasMember("WallsSpeed") && config_doc["WallsSpeed"].IsDouble()){
 		Config.SabersActive = config_doc["SabersActive"].GetBool();
 		Config.LightsActive = config_doc["LightsActive"].GetBool();
+		Config.WallsActive = config_doc["WallsActive"].GetBool();
 		Config.SaberASpeed = config_doc["SaberASpeed"].GetDouble();
 		Config.SaberBSpeed = config_doc["SaberBSpeed"].GetDouble();
 		Config.SabersStartDiff = config_doc["SabersStartDiff"].GetDouble();
 		Config.LightASpeed = config_doc["LightASpeed"].GetDouble();
 		Config.LightBSpeed = config_doc["LightBSpeed"].GetDouble();
 		Config.LightsStartDiff = config_doc["LightsStartDiff"].GetDouble();
+		Config.WallsSpeed = config_doc["WallsSpeed"].GetDouble();
 		log(INFO, "Loaded Configuration!");
 		return true;
 	}
@@ -219,13 +274,17 @@ __attribute__((constructor)) void lib_main()
     #ifdef __aarch64__
     log(INFO, "Is 64 bit!");
     #endif
-    INSTALL_HOOK(SaberBurnMarkSparkles_LateUpdate);
 	
+	INSTALL_HOOK(TutorialController_Awake);
+	INSTALL_HOOK(TutorialController_OnDestroy);
+	INSTALL_HOOK(SaberBurnMarkSparkles_LateUpdate);
 	if(!loadConfig())
 		 createDefaultConfig();
 	saberB = Config.SabersStartDiff;
 	environmentColor1 = Config.LightsStartDiff;
 	if(Config.SabersActive)
 		INSTALL_HOOK(GameNoteController_Update);
+	if(Config.WallsActive)
+		INSTALL_HOOK(ObstacleController_Update);
     log(DEBUG, "Successfully installed RainbowSabers!");
 }

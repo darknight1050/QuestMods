0ae456c93d63d6b3614996d77c66668e58c7343a
darknight1050 <darkdeveloping1050@gmail.com>
2019-10-21 07:45:02 +0200

    Update 0.0.2

--- RainbowSabers/main.cpp
+++ RainbowSabers/main.cpp
@@ -16,7 +16,7 @@
 #define LOG_LEVEL CRITICAL | ERROR | WARNING | INFO | DEBUG
 
 #define MOD_ID "RainbowSabers"
-#define VERSION "0.0.1"
+#define VERSION "0.0.2"
 
 #include "../beatsaber-hook/shared/utils/utils.h"
 #include "../beatsaber-hook/shared/utils/typedefs.h"

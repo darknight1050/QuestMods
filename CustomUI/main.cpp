e28ec6ced4a091f42f3c0629c9a1ce6ae61405d2
darknight1050 <darkdeveloping1050@gmail.com>
2019-10-31 21:15:57 +0100

    Updated CustomUI

--- CustomUI/main.cpp
+++ CustomUI/main.cpp
@@ -22,38 +22,57 @@ using namespace std;
 
 static IL2CPP_Helper* helper;
 
-Il2CppObject* modPanel;
-Il2CppObject* customUIObject;
-bool pressed = false;
+static Il2CppObject* assetBundle;
 
-void ButtonTestOnClick(Il2CppObject* button){
-    log(INFO, "ButtonTestOnClick!");
-    Il2CppObject* textObject = UnityHelper::GetComponentInChildren(helper, customUIObject, helper->GetClassFromName("TMPro", "TextMeshProUGUI"), "TestText");
-    pressed = !pressed;
-    if(pressed){
-         helper->RunMethod(textObject, "set_text", helper->createcsstr("Pressed"));
-    }else{
-        helper->RunMethod(textObject, "set_text", helper->createcsstr("Nope"));
-    }
+static Il2CppObject* modPanel;
+static Il2CppObject* customUIObject;
+
+static int counter = 0;
+
+void UpdateTextCounter(){
+    char text[64];
+    sprintf(text, "Clicks: %d", counter);
+    Il2CppObject* textObject = UnityHelper::GetComponentInChildren(helper, customUIObject, helper->GetClassFromName("TMPro", "TextMeshProUGUI"), "TextCounter");
+    helper->RunMethod(textObject, "set_text", helper->createcsstr(text));
 }
 
-void AssetLoaderOnFinish(Il2CppObject* customUIAsset){
-    helper->RunMethod(&customUIObject, nullptr, helper->class_get_method_from_name(helper->GetClassFromName("UnityEngine", "Object"), "Instantiate", 1), customUIAsset);
+
+void ButtonCounterClick(Il2CppObject* button){
+    log(INFO, "ButtonCounterClick!");
+    counter++;
+    UpdateTextCounter();
+}
+
+void OnLoadAssetComplete(Il2CppObject* asset){
+    helper->RunMethod(&customUIObject, nullptr, helper->class_get_method_from_name(helper->GetClassFromName("UnityEngine", "Object"), "Instantiate", 1), asset);
     UnityHelper::SetParent(helper, customUIObject, QuestUI::GetQuestUIModInfo().Panel);
 
-    UnityHelper::AddButtonOnClick(helper, QuestUI::GetQuestUIInfo()->ButtonBinder, customUIObject, "ButtonTest", (void*)ButtonTestOnClick);
+    UnityHelper::AddButtonOnClick(helper, QuestUI::GetQuestUIInfo()->ButtonBinder, customUIObject, "ButtonCounter", (UnityHelper::ButtonOnClickFunction*)ButtonCounterClick);
+
+    UpdateTextCounter();
+    
     log(INFO, "AssetLoaderOnFinish Called!");
 }
 
-void QuestUIOnInitialized(){
-    UnityAssetLoader::LoadFromFileAsync("/sdcard/Android/data/com.beatgames.beatsaber/files/uis/customUI.qui", (UnityAssetLoader_OnFinishFunction*)AssetLoaderOnFinish);
+void OnLoadAssetBundleComplete(Il2CppObject* assetBundleArg){
+    assetBundle = assetBundleArg;
+    UnityAssetLoader::LoadAssetFromAssetBundleAsync(assetBundle, (UnityAssetLoader_OnLoadAssetCompleteFunction*)OnLoadAssetComplete);
 }
+
+void QuestUIOnInitialized(){
+    if(assetBundle == nullptr){
+        UnityAssetLoader::LoadAssetBundleFromFileAsync("/sdcard/Android/data/com.beatgames.beatsaber/files/uis/customUI.qui", (UnityAssetLoader_OnLoadAssetBundleCompleteFunction*)OnLoadAssetBundleComplete);
+    }else{
+        UnityAssetLoader::LoadAssetFromAssetBundleAsync(assetBundle, (UnityAssetLoader_OnLoadAssetCompleteFunction*)OnLoadAssetComplete);
+    }
+}
+
 void InitHooks(){
     sleep(1);
     helper = new IL2CPP_Helper();
     helper->Initialize();
     
-    QuestUI::Initialize("CustomUI", (void*)QuestUIOnInitialized);
+    QuestUI::Initialize("CustomUI", QuestUIOnInitialized);
     
     log(INFO, "Successfully installed CustomUI!");
 }
